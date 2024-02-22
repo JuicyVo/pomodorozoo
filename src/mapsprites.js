0f2658417a0abc0d435f35b4206069f1d622a782
William <Williamtviet@hotmail.com>
2024-02-21 19:07:41 -0800

    more sprites

--- src/mapsprites.js
+++ src/mapsprites.js
@@ -1,3 +1,4 @@
+// mapsprites.js
 
 import React from 'react';
 import PikachuImage from './images/sprites/pikachu1.gif';
@@ -11,6 +12,13 @@ import RayquazaImage from './images/sprites/rayquaza.gif';
 import LugiaImage from './images/sprites/lugia.gif';
 import GyaradosImage from './images/sprites/gyarados.gif';
 import WailordImage from './images/sprites/wailord.gif';
+import PidgeyImage from './images/sprites/pidgey.gif';
+import PidgeotImage from './images/sprites/pidgeot.gif';
+import PiplupImage from './images/sprites/piplup.gif';
+import MudkipImage from './images/sprites/mudkip.gif';
+import SkarmoryImage from './images/sprites/skarmory.gif'; 
+import OnixImage from './images/sprites/onix.gif'; 
+import ZubatImage from './images/sprites/zubat.gif'; 
 import './MapSprites.css';
 
 function MapSprites() {
@@ -27,6 +35,13 @@ function MapSprites() {
       <img src={LugiaImage} alt="Lugia" className="pokemon lugia" />
       <img src={GyaradosImage} alt="Gyarados" className="pokemon gyarados" />
       <img src={WailordImage} alt="Wailord" className="pokemon wailord" />
+      <img src={PidgeyImage} alt="Pidgey" className="pokemon pidgey" />
+      <img src={PidgeotImage} alt="Pidgeot" className="pokemon pidgeot" />
+      <img src={PiplupImage} alt="Piplup" className="pokemon piplup" />
+      <img src={MudkipImage} alt="Mudkip" className="pokemon mudkip" />
+      <img src={SkarmoryImage} alt="Skarmory" className="pokemon skarmory" /> 
+      <img src={OnixImage} alt="Onix" className="pokemon onix" /> 
+      <img src={ZubatImage} alt="Zubat" className="pokemon zubat" /> 
     </div>
   );
 }
