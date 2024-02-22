bf7dfc9a570499ece7d6379ad0712bcd7bba9492
William <Williamtviet@hotmail.com>
2024-02-21 17:33:39 -0800

    more sprites added

--- src/mapsprites.js
+++ src/mapsprites.js
@@ -1,10 +1,17 @@
-// mapsprites.js
 
 import React from 'react';
-import PikachuImage from './images/sprites/pikachu1.gif'; //have to physically import each sadly
+import PikachuImage from './images/sprites/pikachu1.gif';
 import CharizardImage from './images/sprites/charizard1.gif';
 import MagikarpImage from './images/sprites/magikarp1.gif';
-import './MapSprites.css'; 
+import BlastoiseImage from './images/sprites/blastoise.gif';
+import VenusaurImage from './images/sprites/venusaur.gif';
+import SnorlaxImage from './images/sprites/snorlax.gif';
+import MewtwoImage from './images/sprites/mewtwo.gif';
+import RayquazaImage from './images/sprites/rayquaza.gif';
+import LugiaImage from './images/sprites/lugia.gif';
+import GyaradosImage from './images/sprites/gyarados.gif';
+import WailordImage from './images/sprites/wailord.gif';
+import './MapSprites.css';
 
 function MapSprites() {
   return (
@@ -12,6 +19,14 @@ function MapSprites() {
       <img src={PikachuImage} alt="Pikachu" className="pokemon pikachu" />
       <img src={CharizardImage} alt="Charizard" className="pokemon charizard" />
       <img src={MagikarpImage} alt="Magikarp" className="pokemon magikarp" />
+      <img src={BlastoiseImage} alt="Blastoise" className="pokemon blastoise" />
+      <img src={VenusaurImage} alt="Venusaur" className="pokemon venusaur" />
+      <img src={SnorlaxImage} alt="Snorlax" className="pokemon snorlax" />
+      <img src={MewtwoImage} alt="Mewtwo" className="pokemon mewtwo" />
+      <img src={RayquazaImage} alt="Rayquaza" className="pokemon rayquaza" />
+      <img src={LugiaImage} alt="Lugia" className="pokemon lugia" />
+      <img src={GyaradosImage} alt="Gyarados" className="pokemon gyarados" />
+      <img src={WailordImage} alt="Wailord" className="pokemon wailord" />
     </div>
   );
 }
